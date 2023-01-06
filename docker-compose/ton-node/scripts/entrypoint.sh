4e9eb398de8000fea1de77c581233f81042baa32
igor9105 <igorsam2014@gmail.com>
2023-01-07 00:54:31 +0300

    remove cron and install add java

--- docker-compose/ton-node/scripts/entrypoint.sh
+++ docker-compose/ton-node/scripts/entrypoint.sh
@@ -25,36 +25,11 @@ function f_get_ton_global_config_json() {
     curl -sS "https://raw.githubusercontent.com/tonlabs/main.ton.dev/master/configs/ton-global.config.json" -o "${TON_NODE_CONFIGS_DIR}/ton-global.config.json"
 }
 
-function f_iscron() {
-    if ! grep "validator.sh" /etc/crontab >/dev/null 2>&1; then
-        {
-            echo "RUST_NET_ENABLE=yes"
-            echo "VALIDATOR_NAME=\"${VALIDATOR_NAME}\""
-            echo "SDK_URL=\"${SDK_URL}\""
-            echo "SDK_ENDPOINT_URL_LIST=\"${SDK_ENDPOINT_URL_LIST}\""
-            echo "ELECTOR_TYPE=\"${ELECTOR_TYPE}\""
-        } >>/etc/crontab
-        if [ "${DEPOOL_ENABLE}" = "yes" ]; then
-            {
-                echo "DEPOOL_ENABLE=yes"
-                echo "* * * * *    root  ${TON_NODE_SCRIPTS_DIR}/send_depool_tick_tock.sh >>${TON_NODE_LOGS_DIR}/send_depool_tick_tock.log 2>&1"
-                echo "*/5 * * * *     root  ${TON_NODE_SCRIPTS_DIR}/validator.sh >>${TON_NODE_LOGS_DIR}/validator.log 2>&1"
-            } >>/etc/crontab
-        else
-            {
-                echo "STAKE=$STAKE"
-                echo "*/2 * * * *  root  ${TON_NODE_SCRIPTS_DIR}/validator.sh \${STAKE} >>${TON_NODE_LOGS_DIR}/validator.log 2>&1"
-            } >>/etc/crontab
-        fi
-    fi
 
-    chmod +x ${TON_NODE_SCRIPTS_DIR}/validator.sh
-    pgrep cron >/dev/null || cron
-}
 
 # main
-apt update && apt install -y cron jq wget
-[ "$2" = "validate" ] && f_iscron
+apt update && apt install -y jq wget default-jre
+[ "$2" = "validate" ]
 f_get_ton_global_config_json
 
 if [ "$1" = "bash" ]; then
