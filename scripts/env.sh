017477e2b09c9ea9a3a12442971310baa9f0e020
igor9105 <igorsam2014@gmail.com>
2023-10-16 03:28:02 +0300

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -21,7 +21,7 @@ export TON_NODE_GITHUB_COMMIT_ID="286132d4a22782fd67756fb39cb81b272be9681d"
 
 export TON_NODE_TOOLS_GITHUB_REPO="https://github.com/tonlabs/ton-labs-node-tools.git"
 #export TON_NODE_TOOLS_GITHUB_COMMIT_ID="master"
-export TON_NODE_TOOLS_GITHUB_COMMIT_ID="f9538f208ecc3961c1999ebcfc31003e0b193421"
+export TON_NODE_TOOLS_GITHUB_COMMIT_ID="c2dca0def299d90f1286c3d73ad4f569961343c8"
 
 export TONOS_CLI_GITHUB_REPO="https://github.com/tonlabs/tonos-cli.git"
 export TONOS_CLI_GITHUB_COMMIT_ID="master"
